1006e6c7178206a4cc1e78480e376504d55c943e
Prashant Rahul <prashantrahul141@protonmail.com>
2026-06-03 22:24:10 +0530

    feat: aarch64 wrappers: r_mpidr_el1, r_daif, r_intrd_enabled, w_intrd_enable, w_intrd_disable
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- kernel/aarch64.c
+++ kernel/aarch64.c
@@ -1,6 +1,45 @@
 #include "aarch64.h"
+#include "ktypes.h"
+#include "printf.h"
 
+/* aarch64 break */
 inline void brk(void)
 {
 	asm volatile("brk #0");
+}
+
+/* read mpidr_el1 */
+inline u64 r_mpidr_el1(void)
+{
+	u64 mpidr;
+	asm volatile("mrs %0, MPIDR_EL1" : "=r"(mpidr));
+	return mpidr;
+}
+
+/* read daif  */
+inline u64 r_daif(void)
+{
+	u64 daif;
+	asm volatile("mrs %0, daif" : "=r"(daif));
+	return daif;
+}
+
+/* are device interrupts enabled? */
+inline bool r_intrd_enabled(void)
+{
+	u64 daif = r_daif();
+	u64 masked = (daif & (DAIF_F | DAIF_I));
+	return masked == 0;
+}
+
+/* disable device interrupts */
+inline void w_intrd_disable(void)
+{
+	asm volatile("msr daifset, #0b0011");
+}
+
+/* enable device interrupts */
+inline void w_intrd_enable(void)
+{
+	asm volatile("msr daifclr, #0b0011");
 }
--- kernel/aarch64.h
+++ kernel/aarch64.h
@@ -1,6 +1,27 @@
 #ifndef _ARCH64_H_
 #define _ARCH64_H_
 
+#include "ktypes.h"
+
+#define DAIF_I (1 << 7)
+#define DAIF_F (1 << 6)
+
+/* break */
 void brk(void);
+
+/* read mpidr_el1 */
+u64 r_mpidr_el1(void);
+
+/* read daif */
+u64 r_daif(void);
+
+/* are device interrupts enabled? */
+bool r_intrd_enabled(void);
+
+/* disable device interrupts */
+void w_intrd_disable(void);
+
+/* enable device interrupts */
+void w_intrd_enable(void);
 
 #endif // _ARCH64_H_
