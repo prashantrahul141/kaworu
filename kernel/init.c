5a418061ee4fe43efe45b8cb80dd34cc31440328
Prashant Rahul <prashantrahul141@protonmail.com>
2026-07-05 20:26:06 +0530

    init: clean up unneeded logs
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- kernel/init.c
+++ kernel/init.c
@@ -15,18 +15,7 @@ void kernel_main(void)
 	vm_mem_init();
 	console_init(CONSOLE_BACKEND_UART);
 
-	DEBUG("Hello from %s", "kaworu");
-	INFO("character c = %c", 'c');
-	WARN("character A as ascii = %d", 'A');
-	ERROR("10 = %d", 10);
-	FATAL("17 in hex is %x", 35);
-	DEBUG("203 in hex is %p", 203);
-	INFO("1.0 = %f", 1.0);
-	WARN("-0.0 = %f", -0.0);
-	ERROR("2332323.23232323 = %f", 2332323.23232323);
-	FATAL("1.99999999999 = %f", 1.99999999999);
-	DEBUG("-1.99999999999 = %f", -1.99999999999);
-	INFO("-1.123456 = %f", -1.123456);
+	printf("\nHello from kaworu\n\n");
 
 	printf_deinit();
 	console_deinit();
