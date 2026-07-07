99b22bba8584acd4619325919a5cf0cdc8b7083b
Prashant Rahul <prashantrahul141@protonmail.com>
2026-07-07 19:51:22 +0530

    lib: common_defs: add div_round_up
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- lib/include/common_defs.h
+++ lib/include/common_defs.h
@@ -8,6 +8,7 @@
 #define __round_mask(x, y) ((__typeof__(x))((y) - 1))
 #define round_up(x, y)	   ((((x) - 1) | __round_mask(x, y)) + 1)
 #define round_down(x, y)   ((x) & ~__round_mask(x, y))
+#define div_round_up(n, d) (((n) + (d) - 1) / (d))
 
 /* compiler attributes */
 #define NONNULL(...)   __attribute__((nonnull(__VA_ARGS__)))
