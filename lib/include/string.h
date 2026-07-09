f92c8396d7396085233d1977ec01b8c5b37dc4ca
Prashant Rahul <prashantrahul141@protonmail.com>
2026-07-10 04:10:33 +0530

    lib: add bunch of memory and other libc functions for libfdt to work
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- lib/include/string.h
+++ lib/include/string.h
@@ -11,8 +11,7 @@
 void *memset(void *s, u8 byte, usize count) NONNULL(1);
 
 /*
- * copies size bytes from src to destination.
- * returns original dest pointer.
+ * copies size bytes from src to destination. returns original dest pointer.
  */
 void *memcpy(void *dest, const void *src, usize size) NONNULL(1, 2);
 
@@ -28,5 +27,38 @@ i32 memcmp(const void *s1, const void *s2, usize n);
  * Size of string
  */
 usize strlen(const i8 *s);
+
+/*
+ * size of fixed size string
+ */
+usize strnlen(const i8 *s, usize maxlen);
+
+/*
+ * copies memory area
+ */
+void *memmove(void *dest, const void *src, usize len);
+
+/* scans first n bytes of src for character c
+ */
+void *memchr(const void *src, i32 c, usize n);
+
+/*
+ * scan memory for a character, reversed
+ */
+void *memrchr(const void *m, i32 c, usize n);
+
+/*
+ * locate character in string, reversed
+ */
+i8 *strrchr(const i8 *s, i32 c);
+
+/*
+ * string search character or NULL
+ */
+i8 *strchrnul(const i8 *s, i32 c);
+
+/* locate character in string
+ */
+i8 *strchr(const i8 *s, i32 c);
 
 #endif
