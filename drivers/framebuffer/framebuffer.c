33c439df48cc8c3f9e62d9bc8fc0857b41262a95
Prashant Rahul <prashantrahul141@protonmail.com>
2026-07-18 19:06:04 +0530

    driver: framebuffer: refactor for the new driver model
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- drivers/framebuffer/framebuffer.c
+++ drivers/framebuffer/framebuffer.c
@@ -7,67 +7,65 @@
 #include "flanterm_backends/fb.h"
 #include "io/console.h"
 #include "limine.h"
+#include "manager/manager.h"
 
-typedef struct {
-	ConsoleBackend backend;
-	struct flanterm_context *flanterm_ctx;
-} FramebufferConsoleBackend;
-
-static const ConsoleBackendOps framebuffer_ops = {
+static const ConsoleOps framebuffer_ops = {
 	.read = nullptr,
 	.write = framebuffer_write_event,
 	.flush = framebuffer_flush,
 };
 
-static FramebufferConsoleBackend framebuffer_backend = {
-	.backend = { .name = "framebuffer",
-		     .ops = &framebuffer_ops,
-		     .next = nullptr },
-	.flanterm_ctx = nullptr
+static Device framebuffer_device = {
+	.name = "framebuffer",
+	.console_ops = &framebuffer_ops,
+	.class = DEVICE_FRAMEBUFFER,
 };
+
+static ConsoleBackend framebuffer_backend = { .device = &framebuffer_device,
+					      .next = nullptr };
 
 static struct flanterm_context *flanterm_init(void);
 
 errno_t framebuffer_init()
 {
 	DEBUG("setting up flanterm");
-	framebuffer_backend.flanterm_ctx = flanterm_init();
-	if (IS_ERR(framebuffer_backend.flanterm_ctx)) {
-		panic("failed to init framebuffer");
+	framebuffer_device.driver_data = flanterm_init();
+	if (IS_ERR(framebuffer_device.driver_data)) {
+		ERROR("failed to init framebuffer");
+		return -EINVAL;
 	}
-	console_register(&framebuffer_backend.backend);
+	console_register_backend(&framebuffer_backend);
 	return EOK;
 }
 
 errno_t framebuffer_deinit()
 {
 	DEBUG("removing flanterm");
-	if (nullptr == framebuffer_backend.flanterm_ctx) {
+	if (nullptr == framebuffer_device.driver_data) {
 		panic("failed to deinit framebuffer, not initialised");
 	}
-	console_unregister(&framebuffer_backend.backend);
+	console_unregister(&framebuffer_device);
 	return EOK;
 }
 
-void framebuffer_write_event(ConsoleBackend *backend, const ConsoleEvent *event)
+void framebuffer_write_event(Device *backend, const IOEvent *event)
 {
 	UNUSED_ARG(backend);
-	flanterm_set_text_bg(framebuffer_backend.flanterm_ctx, event->bg.color,
+	flanterm_set_text_bg(framebuffer_device.driver_data, event->bg.color,
 			     event->bg.bright);
 
-	flanterm_set_text_fg(framebuffer_backend.flanterm_ctx, event->fg.color,
+	flanterm_set_text_fg(framebuffer_device.driver_data, event->fg.color,
 			     event->fg.bright);
 
-	flanterm_write(framebuffer_backend.flanterm_ctx, event->msg,
-		       event->len);
-	flanterm_reset_text_bg(framebuffer_backend.flanterm_ctx);
-	flanterm_reset_text_fg(framebuffer_backend.flanterm_ctx);
+	flanterm_write(framebuffer_device.driver_data, event->msg, event->len);
+	flanterm_reset_text_bg(framebuffer_device.driver_data);
+	flanterm_reset_text_fg(framebuffer_device.driver_data);
 }
 
-void framebuffer_flush(ConsoleBackend *backend)
+void framebuffer_flush(Device *backend)
 {
 	UNUSED_ARG(backend);
-	flanterm_flush(framebuffer_backend.flanterm_ctx);
+	flanterm_flush(framebuffer_device.driver_data);
 }
 
 static struct flanterm_context *flanterm_init(void)
